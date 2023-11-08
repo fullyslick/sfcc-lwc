d7a46956bc315507a2f13fbb9b23f9bc5b493f04
A. Rashkov <alexander.rashkov.alfa@gmail.com>
2023-11-08 15:37:31 +0200

    Lifecycle Hooks reference

--- force-app/main/default/lwc/app/app.js
+++ force-app/main/default/lwc/app/app.js
@@ -9,7 +9,9 @@ export default class App extends LightningElement {
     'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
   ready = false;
 
-  // Callback when component is rendered
+  // connectedCallback - Lifecycle hook called when component is added to the DOM
+  // Here is the list of all lifecycle hooks:
+  // https://developer.salesforce.com/docs/platform/lwc/guide/reference-lifecycle-hooks.html
   /* eslint-disable @lwc/lwc/no-async-operation */
   connectedCallback() {
     setTimeout(() => {
