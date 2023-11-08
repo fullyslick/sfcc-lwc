6c94774046481818e5c5bfb640cc73dd27613b1d
A. Rashkov <alexander.rashkov.alfa@gmail.com>
2023-11-08 16:16:55 +0200

    Expose properties from parent to child component

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,6 +1,10 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
+  // One of the usages of a decorator is to expose properties from parent to child
+  // https://developer.salesforce.com/docs/platform/lwc/guide/reactivity-public.html
+  @api productName;
+
   greeting = 'Aleksandar';
 
   changeHandler(event) {
