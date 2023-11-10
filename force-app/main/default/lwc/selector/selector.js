1a2919304971ad7bb175ebbfa0201e8b32ba741e
A. Rashkov <alexander.rashkov.alfa@gmail.com>
2023-11-10 12:45:39 +0200

    Get Salesforce data

--- force-app/main/default/lwc/selector/selector.js
+++ force-app/main/default/lwc/selector/selector.js
@@ -1,11 +1,36 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 
+// adapterId and adapter-modules for getRecord and getFieldValue from lightning/uiRecordApi
+// These are functions that are provided to @wire decorator,
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+
+// imports the current User Id using the @salesforce module
+import Id from '@salesforce/user/Id';
+
+// the @salesforce schema for User.Name.
+// This helps you extract the username specifically from a record with data
+import NAME_FIELD from '@salesforce/schema/User.Name';
+
+const fields = [NAME_FIELD];
 export default class Selector extends LightningElement {
   selectedProductId;
 
-  // 10. Handle emitted event from child 'list' component,
-  // extract the data from event and change 'selectedProductId' so it will update 'detail' component
   handleProductSelected(evt) {
     this.selectedProductId = evt.detail;
+  }
+
+  // Make the user id available as reactive property so it can be rendered on UI
+  userId = Id;
+
+  // Calls decorator with the getRecord function that will extract the data for field = username
+  // After that data is assigned to reactive property so it can be rendered on UI
+  @wire(getRecord, { recordId: '$userId', fields })
+  user;
+
+  // The data for username is an object
+  // This getter allows when accessing {name} in html,
+  // to extract the value of 'username' field
+  get name() {
+    return getFieldValue(this.user.data, NAME_FIELD);
   }
 }
