38be9f95ff7f77109132ce12363ddfd7527afda8
A. Rashkov <alexander.rashkov.alfa@gmail.com>
2023-11-16 15:45:22 +0200

    Get data from SFCC by calling Apex method imperatively

---
+++ force-app/main/default/lwc/callApexImperative/callApexImperative.js
@@ -0,0 +1,41 @@
+import { LightningElement } from 'lwc';
+
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+import REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
+import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
+
+// Import Apex Method
+import getAccounts from '@salesforce/apex/AccountController.getAccounts';
+
+// Configuration used by lightning-datatable to display data
+const COLUMNS = [
+  { label: 'Account Name', fieldName: NAME_FIELD.fieldApiName, type: 'text' },
+  {
+    label: 'Annual Revenue',
+    fieldName: REVENUE_FIELD.fieldApiName,
+    type: 'currency'
+  },
+  { label: 'Industry', fieldName: INDUSTRY_FIELD.fieldApiName, type: 'text' }
+];
+
+export default class AccountList extends LightningElement {
+  // Property we pass to lightning-datatable component
+  // The component provides convenient UI to render data records
+  columns = COLUMNS;
+
+  // We assign the data obtained from Apex method call to this property
+  accounts;
+
+  handleButtonClick() {
+    // Call Apex function on click. It returns a promise resolved with the data or rejected with error
+    getAccounts()
+      .then((accounts) => {
+        //code to execute if accounts are returned successfully
+        this.accounts = accounts;
+      })
+      .catch((error) => {
+        //code to execute if accounts are not returned successfully
+        console.log(error);
+      });
+  }
+}
